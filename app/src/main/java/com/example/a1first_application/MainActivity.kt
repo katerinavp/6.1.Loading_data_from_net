6b13572d7e4e61762ec18b986e119bd214d3f4ac
Kate <bl_ev@mail.ru>
2021-02-09 21:35:47 +0300

    Изменение CoroutineScope(IO).launch на lifecycleScope.launch.

--- app/src/main/java/com/example/a1first_application/MainActivity.kt
+++ app/src/main/java/com/example/a1first_application/MainActivity.kt
@@ -3,22 +3,17 @@ package com.example.a1first_application
 import android.os.Bundle
 import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.a1first_application.databinding.ActivityMainBinding
 import io.ktor.client.*
 import io.ktor.client.features.json.*
 import io.ktor.client.request.*
 import io.ktor.http.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers.IO
-import kotlinx.coroutines.Dispatchers.Main
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import kotlin.coroutines.EmptyCoroutineContext
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var adapterPost: AdapterPost
     private val url = "https://raw.githubusercontent.com/katerinavp/GSON/master/posts.json"
     lateinit var adapter: AdapterPost
     private val binding by lazy(LazyThreadSafetyMode.NONE) {
@@ -33,7 +28,7 @@ class MainActivity : AppCompatActivity() {
         adapter = AdapterPost()
         binding.recyclerView.adapter = adapter
 
-        CoroutineScope(IO).launch {
+        lifecycleScope.launch {
             getResultFromGit()
         }
 
@@ -42,31 +37,18 @@ class MainActivity : AppCompatActivity() {
     private suspend fun getResultFromGit() {
         val client = HttpClient {
             install(JsonFeature) {
-                // объясним чуть позже
                 acceptContentTypes = listOf(
-                    ContentType.Text.Plain,
-                    ContentType.Application.Json
+                        ContentType.Text.Plain,
+                        ContentType.Application.Json
                 )
                 serializer = GsonSerializer()
             }
         }
-        with(CoroutineScope(EmptyCoroutineContext))
-        {
-            launch {
-                // тестовый ответ будет десериализован в List<Post>
-                val response = client.get<List<Post>>(url)
-                println("Десериализация + ${response}")
-                client.close()
-                setResponseOnMainThread(response)
-            }
-        }
-    }
-
-    private suspend fun setResponseOnMainThread(response: List<Post>) {
-        withContext(Main) {
-            setResponse(response)
-            println("Главный поток + $response")
-        }
+        // тестовый ответ будет десериализован в List<Post>
+        val response = client.get<List<Post>>(url)
+        println("Десериализация + ${response}")
+        client.close()
+        setResponse(response)
     }
 
     private fun setResponse(response: List<Post>) {
